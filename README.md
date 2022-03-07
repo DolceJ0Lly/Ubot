aeeb823d54dd06f13913ed7fe587014aa1becddf
Falcone8 <maule2703@gmail.com>
2022-03-07 16:24:19 +0100

    Add new file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Ubot
-Ubot for Telegram Written In Telethon
+# Userbot-personal
+This code is free it is not a code that is very long
+
+
+### Self Host
+- Termux or Heroku
+
+
+## Description
+- The license will not be there
+
+- I will only wear two or three things but I will not put much
+
+
+## Support Group
+
+[Support Group](https://t.me/+jjC0mQf6H2djMzY0)
---
+++ Userbot_personal.py
@@ -0,0 +1,99 @@
+from telethon import TelegramClient, events, sync
+from telethon import Button
+from telethon import functions, types
+from datetime import datetime
+
+import os
+import asyncio
+import telethon
+
+
+API_ID = 1641545
+API_HASH ="5e2f76a9ef8d04cb1386a9f9d246dd9f"
+
+userbot_personal = TelegramClient("userbot_personal", API_ID, API_HASH)
+
+
+
+
+async def usbot():
+
+  
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.status"))
+  async def statususerbot(event):
+    await event.edit("userbot personal online")
+
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.name"))
+  async def nameprofilyou(event):
+    x = event.text.split(" ", maxsplit=1)[1]
+    result = await userbot_personal(functions.account.UpdateProfileRequest(
+          first_name=x,
+      ))
+    await event.edit(f"name put as **{x}**")
+
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.stopuserbot"))
+  async def _stopuserbot(event):
+    await event.edit("fra 3 secondi andra offline l'userbot")
+    await userbot_personal.disconnect()
+    await asyncio.sleep(3)
+
+
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.reboot"))
+  async def reboot(event):
+    await event.delete()
+    import os, sys, threading
+    os.system("clear")
+  
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    thereading.Thread(target=_restart, args=(bot, msg)).start()
+
+
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r".ping"))
+  async def _ping(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("**Pong!**")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit("**Pong!**\n`{}`".format(ms))
+
+  @userbot_personal.on(events.NewMessage(outgoing=True))
+  async def t_p(event):
+    if event.text == ".d":
+      await event.edit("Non è divertente. Non ho riso. Il tuo tentativo di fare il comico è stato talmente pietoso da rendermi ancora più triste e frustrato di prima. La tua idea di scherzo è completamente fallata, le tue battute sono delle offese a livello intellettuale nei confronti di tutti i comici che a differenza tua sanno far ridere la gente. Io mi immagino te, dietro la tastiera, con un sorriso malizioso sulle labbra, pronto a scrivere questa battuta mediocre che non mi ha fatto nemmeno sorridere. Dovresti seriamente smettere di provare a far ridere la gente, perché non fa per te, non ne sei capace, non sei divertente, non sei simp, e le tue battute sono deprimenti e non fanno ridere")
+
+
+
+  #@userbot_personal.on(events.Raw)
+  #async def handler(update):
+    # Print all incoming updates
+    #await userbot_personal.send_message("me",update.stringify())
+
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.delm"))
+  async def _delm(event):
+    await event.delete()
+    d = await event.get_reply_message()
+    await d.delete()
+    await event.respond("deleted message")
+
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.del"))
+  async def _delm(event):
+    await event.delete()
+    d = await event.get_reply_message()
+    await d.delete()
+
+  @userbot_personal.on(events.NewMessage(outgoing=True, pattern=r"\.version_telethon"))
+  async def versiontelethon(e):
+    g = (telethon.__version__)
+    await e.edit(f"version telethon -> {g}")
+
+
+  
+print("Userbot Personal Online!")
+
+
+with userbot_personal:
+  userbot_personal.loop.run_until_complete(usbot())
+  userbot_personal.run_until_disconnected()
+
